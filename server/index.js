4f4eed667dfb2863a8c107210ed8fe3a332f4a5c
HineshKumar1 <hineshkumar46@gmail.com>
2023-11-17 00:54:00 +0500

    connect to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan")
 require("dotenv").config();
+const colors = require("colors");
+const connectDB = require("./config/db.js")
 
+
+//middleware
+app.use(express.json())
+app.use(morgan("dev"))
+
+//Connection to Databse
+connectDB();
+
+//Port Configuration
 const port = process.env.PORT || 5000
 
 app.listen(port,()=>{
