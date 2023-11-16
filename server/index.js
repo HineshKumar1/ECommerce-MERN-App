3cd3bd8f5cb35bd7f34e365a50dbcda7ff82e963
HineshKumar1 <hineshkumar46@gmail.com>
2023-11-17 00:14:44 +0500

    create server for backend

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>{
+    console.log(`Server is Lisiting on port ${port}`);
+})
