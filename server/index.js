b3eb2e42b9c496768e6a833bd54719de9cf03a31
Hinesh Kumar <65116491+HineshKumar1@users.noreply.github.com>
2024-04-18 17:21:44 +0500

    Add static file serving for client build

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const cors = require("cors")
 const colors = require("colors");
 const connectDB = require("./config/db.js")
 const category = require("./routes/category.js")
+const path = require("path");
 
 //middleware
 app.use(cors())
 app.use(express.json())
-app.use(morgan("dev"))
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build"))) ;
 
 //Connection to Databse
 connectDB();
@@ -20,7 +22,12 @@ const port = process.env.PORT || 5000
 
 app.use("/user",require("./routes/user.js"))
 app.use("/category",category);
-app.use("/product",require("./routes/product.js"))
+app.use("/product",require("./routes/product.js"));
+
+//rest API
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
 
 app.listen(port,()=>{
     console.log(`Server is Lisiting on port ${port}`);
